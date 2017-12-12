c0e32d48e82bbc6250bc1cb9813d27214150b886
Mateusz Nowak <kontakt.mateusznowak@gmail.com>
2017-12-12 20:46:52 +0100

    Part 1 Finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { InputTweetComponent } from './components/input-tweet/input-tweet.component';
+import { TweetComponent } from './components/tweet/tweet.component';
+import { WallComponent } from './components/wall/wall.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputTweetComponent,
+    TweetComponent,
+    WallComponent
   ],
   imports: [
     BrowserModule
